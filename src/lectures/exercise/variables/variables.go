cf1a043d2138d5b5a75dea081d59ef430e80ae0d
marc-poljak <55477845+marc-poljak@users.noreply.github.com>
2024-05-05 09:12:29 +0200

    Exercise: Variables

--- src/lectures/exercise/variables/variables.go
+++ src/lectures/exercise/variables/variables.go
@@ -25,4 +25,26 @@ package main
 import "fmt"
 
 func main() {
+	//* Store your favorite color in a variable using the `var` keyword
+	var favoriteColor = "green"
+	fmt.Println("My favorite color is", favoriteColor)
+
+	//* Store your birth year and age (in years) in two variables using
+	//  compound assignment
+	birthYear, ageInYears := 1900, 124
+	fmt.Println("Born in", birthYear, "aged", ageInYears, "years")
+
+	//* Store your first & last initials in two variables using block assignment
+	var (
+		firstInitial = "M"
+		lastInitial  = "P"
+	)
+	fmt.Println("Initials = ", firstInitial, lastInitial)
+
+	//* Declare (but don't assign!) a variable for your age (in days),
+	//  then assign it on the next line by multiplying 365 with the age
+	// 	variable created earlier
+	var ageInDays int
+	ageInDays = 365 * ageInYears
+	fmt.Println("I am", ageInDays, "days old")
 }
